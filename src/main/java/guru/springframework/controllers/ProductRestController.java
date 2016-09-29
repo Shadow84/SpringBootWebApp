44e90b7e7b33cf4354f6bdd856c1399f2366cff7
Serhiy Khomenko <Khomenkosm@gmail.com>
2016-09-30 00:24:33 +0300

    add Product Rest Controller

---
+++ src/main/java/guru/springframework/controllers/ProductRestController.java
@@ -0,0 +1,44 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Product;
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProductRestController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping(value = "rest/products", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Product> list(){
+        return  productService.listAllProducts();
+    }
+
+    @RequestMapping(value = "rest/product/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product showProduct(@PathVariable Integer id){
+        return productService.getProductById(id);
+    }
+
+    @RequestMapping(value = "rest/product/edit/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Product edit(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @RequestMapping(value = "rest/product", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveProduct(Product product){
+        productService.saveProduct(product);
+    }
+
+    @RequestMapping(value = "rest/product/delete/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Integer id){
+        productService.deleteProduct(id);
+    }
+
+}
