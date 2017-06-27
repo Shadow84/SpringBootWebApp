600aabe5a35d0b7792cff496c25b7e2f231c0250
Serhiy Khomenko <Khomenkosm@gmail.com>
2017-06-27 21:44:06 +0300

    add comments

--- src/main/java/com/khomenko/configuration/WebConfiguration.java
+++ src/main/java/com/khomenko/configuration/WebConfiguration.java
@@ -5,6 +5,29 @@ import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/*
+*
+Настройка БД
+http://localhost:8080/console/
+
+По  API (MVC)
+Сделал несколько страничек для CRUD операций,  стартуют вместе с spring boot
+http://localhost:8080/                            -  home
+http://localhost:8080/products                -  get all
+http://localhost:8080/product/{id}             - get one
+http://localhost:8080/product/edit/{1}       - edit one
+http://localhost:8080/product/delete/{1}    - delete one
+
+По REST API
+Сделал  отдельный контроллер,  тестил SOAP UI
+http://localhost:8080/rest/products           GET      - get all
+http://localhost:8080/rest/product/{id}       GET      - get one
+http://localhost:8080/rest/product/{id}       POST    - edit one
+http://localhost:8080/rest/product            PUT       - add one
+http://localhost:8080/rest/product/{id}      DELETE  -delete one
+*
+* */
+
 @Configuration
 public class WebConfiguration {
     @Bean
